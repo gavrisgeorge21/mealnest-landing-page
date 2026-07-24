1e09d12ee0fd761e943e6f047d6fe927a9470159
gavrisgeorge21 <gavrisgeorge350@gmail.com>
2026-07-24 14:54:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   navbarLinks.classList.toggle("active");
 });
+
+const scrollRevealElements = document.querySelectorAll(".scroll-reveal");
+const scrollRevealObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  },
+);
+
+scrollRevealElements.forEach((element) => {
+  scrollRevealObserver.observe(element);
+});
